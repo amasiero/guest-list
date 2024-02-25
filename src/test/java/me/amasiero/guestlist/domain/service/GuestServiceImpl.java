7efc7caa8fa961a8c5aabdb1048417cfccb19554
Andrey Masiero <andrey.masiero@motork.io>
2024-02-25 14:54:24 +0000

    feat(GuestServiceImpl.java): implement GuestService interface test(GuestServiceTest.java): add unit test for createGuest method in GuestService A new class, GuestServiceImpl, has been created to implement the GuestService interface. This is a part of the application's domain layer and is responsible for handling operations related to guests. The createGuest method has been added to this class but currently returns null. This method will be further developed in the future. In addition, a unit test for the createGuest method has been added to the GuestServiceTest class. This test ensures that the method is working as expected and will help prevent regressions in the future.

---
+++ src/test/java/me/amasiero/guestlist/domain/service/GuestServiceImpl.java
@@ -0,0 +1,11 @@
+package me.amasiero.guestlist.domain.service;
+
+import me.amasiero.guestlist.domain.service.dto.Guest;
+import me.amasiero.guestlist.domain.service.ports.input.GuestService;
+
+public class GuestServiceImpl implements GuestService {
+    @Override
+    public Guest createGuest(Guest guest) {
+        return null;
+    }
+}
--- src/test/java/me/amasiero/guestlist/domain/service/ports/input/GuestServiceTest.java
+++ src/test/java/me/amasiero/guestlist/domain/service/ports/input/GuestServiceTest.java
@@ -1,5 +1,34 @@
 package me.amasiero.guestlist.domain.service.ports.input;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import me.amasiero.guestlist.domain.service.GuestServiceImpl;
+import me.amasiero.guestlist.domain.service.dto.Guest;
+import me.amasiero.guestlist.domain.service.dto.mock.GuestDataMock;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ExtendWith(MockitoExtension.class)
 class GuestServiceTest {
+
+    @Spy
+    protected GuestService guestService = new GuestServiceImpl();
+
+    @Nested
+    @DisplayName("when creating a guest")
+    class CreateGuest {
+
+        @Test
+        @DisplayName("should successfully create a guest")
+        public void shouldCreateAGuest() {
+            Guest result = guestService.createGuest(GuestDataMock.build());
+            assertNotNull(result);
+        }
+    }
 
 }
